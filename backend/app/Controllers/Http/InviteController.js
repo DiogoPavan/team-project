039be632035fdb55278da73c2505142d83d21f73
Diogo Pavan <diogopv10@gmail.com>
2020-01-21 21:15:20 -0300

    Incluindo InviteHOok

--- backend/app/Controllers/Http/InviteController.js
+++ backend/app/Controllers/Http/InviteController.js
@@ -1,12 +1,21 @@
 'use strict'
 
+const Invite = use('App/Models/Invite')
+
 class InviteController {
   /**
    * Create/save a new invite.
    * POST invites
    */
-  async store ({ request, response }) {
-    console.log(request.team)
+  async store ({ request, auth }) {
+    const invites = request.input('invites')
+    const data = invites.map(email => ({
+      email,
+      user_id: auth.user.id,
+      team_id: request.team.id
+    }))
+
+    await Invite.createMany(data)
   }
 }
 
